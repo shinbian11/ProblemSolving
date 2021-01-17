b89da1dfe098c43a19b7c80866065f5d924983b7
shinbian11 <shinbian11@gmail.com>
2021-01-17 11:29:02 +0900

    [백준] 1600번 : 말이 되고픈 원숭이 (2차원이 아닌 3차원 배열을 사용한 BFS)

---
+++ bfs_3_dimension_array_movingMonkey.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#define F_I ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pl;
+typedef pair<int, int> pi;
+
+ll Min(ll a, ll b) { return (a < b) ? a : b; }
+ll Max(ll a, ll b) { return (a < b) ? b : a; }
+ll gcd(ll m, ll n) { if (n == 0) return m; return gcd(n, m % n); } //최대공약수
+ll lcm(ll m, ll n) { return m * n / gcd(m, n); } //최소공배수
+
+int dir[2][12] = { {0,0,1,-1, -1,-1,1,1,-2,-2,2,2},{1,-1,0,0,2,-2,-2,2,1,-1,-1,1} };
+
+int cost_near[12] = { 0,0,0,0,1,1,1,1,1,1,1,1 };
+int arr[204][204];
+int d[204][204][32];
+
+int main()
+{
+	//백준 1600번 : 말이 되고픈 원숭이 (2차원이 아닌 3차원 배열을 사용한 BFS)
+	//참고 : 2206번 > 벽 부수고 이동하기
+
+	F_I;
+	int k, n, m;
+	cin >> k;
+	cin >> m >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	memset(d, -1, sizeof(d));
+
+	queue< tuple<int, int, int> >q;
+	q.push(make_tuple(0, 0, 0));
+
+	d[0][0][0] = 0;
+
+	while (!q.empty())
+	{
+		int x, y, cost;
+		tie(x, y, cost) = q.front();
+		q.pop();
+
+		for (int i = 0; i < 12; i++)
+		{
+			int nx = x + dir[0][i];
+			int ny = y + dir[1][i];
+			int ncost = cost + cost_near[i];
+
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m)
+			{
+				if (arr[nx][ny] == 1) continue; //장애물이면 움직일 수 없다.
+
+				if (d[nx][ny][ncost] == -1)
+				{
+					if (ncost <= k) //cost가 전혀 들지 않는 경우(상하좌우만 이동) 과 cost가 1 드는 경우(말처럼 이동)를 모두 포괄한다!
+					{
+						d[nx][ny][ncost] = d[x][y][cost] + 1;
+						q.push(make_tuple(nx, ny, ncost));
+					}
+				}
+
+			}
+		}
+	}
+
+
+
+	int ans = 50000;
+
+	for (int i = 0; i <= k; i++)
+	{
+		if (d[n - 1][m - 1][i] == -1)
+			continue;
+		else
+		{
+			if (ans > d[n - 1][m - 1][i])
+				ans = d[n - 1][m - 1][i];
+		}
+		
+	}
+
+	if (ans == 50000)
+		cout << -1 << '\n';
+	else
+		cout << ans << '\n';
+
+
+	return 0;
+}
